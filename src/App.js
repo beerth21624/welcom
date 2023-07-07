9449da4060800888f4181223c63a7f25fbf3ab16
beerth21624 <beerzii.4321@gmail.com>
2023-07-07 18:57:34 +0700

    edit hint

--- src/App.js
+++ src/App.js
@@ -12,16 +12,31 @@ function App() {
     //show alert
     alert("Copied to clipboard");
   }
+
+  // function if time between 0600 to 0800 return true
+  function checkTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const time = hour * 100 + minute;
+    if (time >= 600 && time <= 800) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <div className="main-bg">
       <sub
         style={{
           position: "absolute",
           bottom: "0",
-          fontSize: "5px",
+          fontSize: "10px",
+          display: checkTime() ? "block" : "none",
         }}
       >
-        by baria_srw
+        baria_srw
       </sub>
 
       <div className="input-container">
@@ -33,14 +48,22 @@ function App() {
             fontWeight: "bold",
           }}
         >
-          สิ่งที่น้องตามหาอยู่ข้างล่างนี้!!!
+          เมื่อถึงเวลาที่เหมาะสมสิ่งที่น้องตามหาจะปรากฏ
         </p>
-        <h1 className="title">Good Luck</h1>
 
+        <h1 className="title">Good Luck</h1>
+        <p
+          style={{
+            fontSize: "20px",
+          }}
+          className="title"
+        >
+          กำลัง งง ใช่มั้ย พี่ใจดีมีคำใบ้เพิ่มเติมด้านล่างนี้
+        </p>
         <div className="input-box">
           <input
             type="text"
-            value="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGVwIjoiZ29CYWNrKCk9PiBhYnNvbHV0ZSA9PiBib3R0b20gPSAwICAifQ.CeLIoEsOgFxV1IucvaoIwB7slV6L7a0ja4WTrNCFd4c"
+            value="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lIjoiMDYwMC0wODAwIiwibG9jYXRpb24iOiJib3R0b20gbGVmdCIsIuC4l-C4suC4h-C4peC4seC4lCI6IndlYiBkZXYg4LmD4LiZ4Lia4Liy4LiH4LiE4Lij4Lix4LmJ4LiH4LmC4LiE4LmJ4LiU4LmA4Lij4Liy4LiB4LmH4Lih4Li14Lib4Lix4LiN4Lir4LiyIn0.6HVb5E6xJ4KHHUDE_3tS5QGkCFZbss2DyKN3zxyNruw"
           />
           <div
             style={{
@@ -51,7 +74,7 @@ function App() {
               className="btn-copy"
               onClick={() =>
                 copyToClipboard(
-                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGVwIjoiZ29CYWNrKCk9PiBhYnNvbHV0ZSA9PiBib3R0b20gPSAwICAifQ.CeLIoEsOgFxV1IucvaoIwB7slV6L7a0ja4WTrNCFd4c"
+                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lIjoiMDYwMC0wODAwIiwibG9jYXRpb24iOiJib3R0b20gbGVmdCIsIuC4l-C4suC4h-C4peC4seC4lCI6IndlYiBkZXYg4LmD4LiZ4Lia4Liy4LiH4LiE4Lij4Lix4LmJ4LiH4LmC4LiE4LmJ4LiU4LmA4Lij4Liy4LiB4LmH4Lih4Li14Lib4Lix4LiN4Lir4LiyIn0.6HVb5E6xJ4KHHUDE_3tS5QGkCFZbss2DyKN3zxyNruw"
                 )
               }
             >
@@ -61,7 +84,8 @@ function App() {
               className="btn-solve"
               onClick={() => {
                 //link
-                window.location.href = "https://www.instagram.com/saacsos/";
+                window.location.href =
+                  "https://www.instagram.com/newjeans_official/";
               }}
             >
               เฉลย
